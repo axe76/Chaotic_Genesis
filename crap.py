98bc14851ca228ee10fad3bf519d6b00254f311b
7enTropy7 <menon.uk1998@gmail.com>
2020-08-24 12:59:18 +0530

    explanation added

--- crap.py
+++ crap.py
@@ -1,6 +1,6 @@
 from fuzzywuzzy import fuzz
 
-fitness = 100-fuzz.ratio('abcde','bcdef')
+fitness = 100-fuzz.ratio('d','bcdef')
 
 print(fitness)
 
@@ -21,4 +21,21 @@ in crossover, we randomly generate offspirings using a1,d2 and a2,d1.
 in mutation, for 10% of times, we need to do something based on steps.
 
 
+x = [0.3,0.997,0.862,0.531,0.761]
+
+
+x_sorted = [0.997,0.862,0.761,0.531,0.3]
+
+[4,0,1,3,2]
+
+[3,4,2,4,6]
+
+[7,4,6,3,1]= key
+[65,66,...]
+
+[cipher]
 '''
+
+
+
+
